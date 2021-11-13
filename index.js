bc6dbb21a4790522b6b02c9409442c8bbda1e2c3
Sheikh Md Obaidullah <obaidullah2p17@gmail.com>
2021-11-13 09:15:50 +0600

    4th commit

--- index.js
+++ index.js
@@ -102,23 +102,6 @@ async function run() {
              res.json(result);
          });
 
-
-        // app.get("/appointments", verifyToken, async (req, res) => {
-        //     const email = req.query.email;
-        //     const date = new Date(req.query.date).toLocaleDateString();
-        //     const query = { email: email, date: date };
-        //     const cursor = appointmentsCollection.find(query);
-        //     const appointments = await cursor.toArray();
-        //     res.json(appointments);
-        // });
-
-        // app.post("/appointments", async (req, res) => {
-        //     const appointment = req.body;
-        //     const result = await appointmentsCollection.insertOne(appointment);
-        //     console.log(result);
-        //     res.json(result);
-        // });
-
         app.get("/users/:email", async (req, res) => {
             const email = req.params.email;
             const query = { email: email };
